fdcfcc8184976ccec7daf970878ab0148726c859
DESKTOP-DMEFEJ6\Andrea <andrea.elias16@gmail.com>
2019-03-03 16:20:09 -0600

    Diseños

---
+++ app/src/main/java/com/example/andrea/lab7/NuevoContacto.kt
@@ -0,0 +1,12 @@
+package com.example.andrea.lab7
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NuevoContacto : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nuevo_contacto)
+    }
+}
