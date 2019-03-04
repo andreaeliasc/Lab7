a2a222869ac4eebdf8528cade24dee9fcb250d41
DESKTOP-DMEFEJ6\Andrea <andrea.elias16@gmail.com>
2019-03-03 18:27:30 -0600

    no message

--- app/src/main/java/com/example/andrea/lab7/EditarContacto.kt
+++ app/src/main/java/com/example/andrea/lab7/EditarContacto.kt
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.example.andrea.lab7.DataBase.Companion.CON
 import kotlinx.android.synthetic.main.activity_editar_contacto.*
 
 class EditarContacto : AppCompatActivity() {
@@ -18,13 +17,13 @@ class EditarContacto : AppCompatActivity() {
     fun edit(view: View){
         val values= ContentValues()
         values.put(DataBase.NOMBRE, NAME.text.toString())
-        values.put(DataBase.CEL, CEL.text.toString())
-        values.put(DataBase.EMAIL, EMAIL.text.toString())
-        MAIN.Cn.set(intent.getIntExtra("item",0), Cn(CEL.text.toString(),NAME.text.toString(),EMAIL.text.toString()))
+        values.put(DataBase.CEL, Phone.text.toString())
+        values.put(DataBase.EMAIL, Mail.text.toString())
+        MAIN.Cn.set(intent.getIntExtra("item",0), Cn(Phone.text.toString(),NAME.text.toString(),Mail.text.toString()))
         Toast.makeText(applicationContext,"Se ha editado el contacto de manera exitosa", Toast.LENGTH_SHORT).show()
         NAME.setText("")
-        CEL.setText("")
-        EMAIL.setText("")
+        Phone.setText("")
+        Mail.setText("")
     }
 }
 
--- app/src/main/java/com/example/andrea/lab7/NuevoContacto.kt
+++ app/src/main/java/com/example/andrea/lab7/NuevoContacto.kt
@@ -1,12 +1,29 @@
 package com.example.andrea.lab7
 
+import android.content.ContentValues
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_editar_contacto.*
+import kotlinx.android.synthetic.main.activity_nuevo_contacto.*
 
 class NuevoContacto : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nuevo_contacto)
+    }
+
+fun addContact(view: View){
+    val values = ContentValues()
+    values.put(DataBase.NOMBRE, NAME.text.toString())
+
+
+}
+    fun REGRESO (view: View){
+        val intent: Intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 }
